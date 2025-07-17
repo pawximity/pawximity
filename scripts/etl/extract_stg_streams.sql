e960e423aeba6fca86a730aa7983f9e0cfc91335
pawximity <chrisloverchio+gis@gmail.com>
2025-07-16 21:26:01 -0600

    staging streams data

---
+++ scripts/etl/extract_stg_streams.sql
@@ -0,0 +1,25 @@
+-- pawximity.stg_streams
+
+-- Drop table
+
+-- DROP TABLE pawximity.stg_streams;
+
+CREATE TABLE pawximity.stg_streams (
+	id SERIAL PRIMARY KEY,             
+	stream_id INTEGER,                 
+	stream_type TEXT,
+	length_km DOUBLE PRECISION,
+	stream_geom GEOMETRY(MultiLineString, 26912)
+);
+
+CREATE INDEX stg_streams_geom_geom_idx ON pawximity.stg_streams USING gist (stream_geom);
+
+-- TRUNCATE TABLE pawximity.stg_streams;
+
+INSERT INTO pawximity.stg_streams (stream_id, stream_type, length_km, stream_geom)
+SELECT ogc_fid,
+       ftype_text,
+       lengthkm,
+       ST_Multi(geom)::geometry(MultiLineString, 26912)
+FROM pawximity.utah_streams_hydrography_ugrc 
+WHERE inutah = 1;
