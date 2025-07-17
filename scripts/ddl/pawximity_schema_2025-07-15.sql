8abb270864e740d3f1b9bf5bfdc9694fc734b916
pawximity <chrisloverchio+gis@gmail.com>
2025-07-16 21:57:54 -0600

    using an external formatter for consistency

--- scripts/ddl/pawximity_schema_2025-07-15.sql
+++ scripts/ddl/pawximity_schema_2025-07-15.sql
@@ -1,7 +1,6 @@
 -- DROP SCHEMA pawximity;
 
 CREATE SCHEMA pawximity AUTHORIZATION postgres;
-
 -- DROP SEQUENCE pawximity.utah_county_boundary_ugrc_ogc_fid_seq;
 
 CREATE SEQUENCE pawximity.utah_county_boundary_ugrc_ogc_fid_seq
@@ -55,15 +54,15 @@ CREATE SEQUENCE pawximity.utah_streams_hydrography_ugrc_ogc_fid_seq
 	MAXVALUE 2147483647
 	START 1
 	CACHE 1
-	NO CYCLE;-- pawximity.utah_county_boundary_ugrc definition
-
+	NO CYCLE;
+-- pawximity.utah_county_boundary_ugrc definition
 -- Drop table
-
 -- DROP TABLE pawximity.utah_county_boundary_ugrc;
 
 CREATE TABLE pawximity.utah_county_boundary_ugrc (
 	ogc_fid serial4 NOT NULL,
-	geom public.geometry(multipolygon, 26912) NULL,
+	geom public.geometry(multipolygon,
+26912) NULL,
 	object_id int4 NULL,
 	countynbr varchar NULL,
 	entitynbr int4 NULL,
@@ -78,18 +77,18 @@ CREATE TABLE pawximity.utah_county_boundary_ugrc (
 	color4 int4 NULL,
 	CONSTRAINT utah_county_boundary_ugrc_pk PRIMARY KEY (ogc_fid)
 );
-CREATE INDEX utah_county_boundary_ugrc_geom_geom_idx ON pawximity.utah_county_boundary_ugrc USING gist (geom);
 
-
+CREATE INDEX utah_county_boundary_ugrc_geom_geom_idx ON
+pawximity.utah_county_boundary_ugrc
+	USING gist (geom);
 -- pawximity.utah_lakes_hydrography_ugrc definition
-
 -- Drop table
-
 -- DROP TABLE pawximity.utah_lakes_hydrography_ugrc;
 
 CREATE TABLE pawximity.utah_lakes_hydrography_ugrc (
 	ogc_fid serial4 NOT NULL,
-	geom public.geometry(multipolygon, 26912) NULL,
+	geom public.geometry(multipolygon,
+26912) NULL,
 	object_id int4 NULL,
 	permanent_identifier varchar NULL,
 	fdate varchar NULL,
@@ -107,42 +106,42 @@ CREATE TABLE pawximity.utah_lakes_hydrography_ugrc (
 	ismajor int4 NULL,
 	CONSTRAINT utah_lakes_hydrography_ugrc_pk PRIMARY KEY (ogc_fid)
 );
-CREATE INDEX utah_lakes_hydrography_ugrc_geom_geom_idx ON pawximity.utah_lakes_hydrography_ugrc USING gist (geom);
 
-
+CREATE INDEX utah_lakes_hydrography_ugrc_geom_geom_idx ON
+pawximity.utah_lakes_hydrography_ugrc
+	USING gist (geom);
 -- pawximity.utah_roads_osm definition
-
 -- Drop table
-
 -- DROP TABLE pawximity.utah_roads_osm;
 
 CREATE TABLE pawximity.utah_roads_osm (
 	ogc_fid serial4 NOT NULL,
-	geom public.geometry(multilinestring, 26912) NULL,
+	geom public.geometry(multilinestring,
+26912) NULL,
 	osm_id varchar(12) NULL,
-	code numeric(4) NULL,
+	code NUMERIC(4) NULL,
 	fclass varchar(28) NULL,
 	"name" varchar(100) NULL,
 	"ref" varchar(20) NULL,
 	oneway varchar(1) NULL,
-	maxspeed numeric(3) NULL,
-	layer numeric(12) NULL,
+	maxspeed NUMERIC(3) NULL,
+	layer NUMERIC(12) NULL,
 	bridge varchar(1) NULL,
 	tunnel varchar(1) NULL,
 	CONSTRAINT utah_roads_osm_pk PRIMARY KEY (ogc_fid)
 );
-CREATE INDEX utah_roads_osm_geom_geom_idx ON pawximity.utah_roads_osm USING gist (geom);
 
-
+CREATE INDEX utah_roads_osm_geom_geom_idx ON
+pawximity.utah_roads_osm
+	USING gist (geom);
 -- pawximity.utah_springs_hydrography_ugrc definition
-
 -- Drop table
-
 -- DROP TABLE pawximity.utah_springs_hydrography_ugrc;
 
 CREATE TABLE pawximity.utah_springs_hydrography_ugrc (
 	ogc_fid serial4 NOT NULL,
-	geom public.geometry(multipoint, 26912) NULL,
+	geom public.geometry(multipoint,
+26912) NULL,
 	object_id int4 NULL,
 	permanent_identifier varchar NULL,
 	fdate varchar NULL,
@@ -157,35 +156,35 @@ CREATE TABLE pawximity.utah_springs_hydrography_ugrc (
 	inutah int4 NULL,
 	CONSTRAINT utah_springs_hydrography_ugrc_pk PRIMARY KEY (ogc_fid)
 );
-CREATE INDEX utah_springs_hydrography_ugrc_geom_geom_idx ON pawximity.utah_springs_hydrography_ugrc USING gist (geom);
 
-
+CREATE INDEX utah_springs_hydrography_ugrc_geom_geom_idx ON
+pawximity.utah_springs_hydrography_ugrc
+	USING gist (geom);
 -- pawximity.utah_state_boundary_ugrc definition
-
 -- Drop table
-
 -- DROP TABLE pawximity.utah_state_boundary_ugrc;
 
 CREATE TABLE pawximity.utah_state_boundary_ugrc (
 	ogc_fid serial4 NOT NULL,
-	geom public.geometry(multipolygon, 26912) NULL,
+	geom public.geometry(multipolygon,
+26912) NULL,
 	object_id int4 NULL,
 	state varchar NULL,
 	global_id varchar NULL,
 	CONSTRAINT utah_state_boundary_ugrc_pk PRIMARY KEY (ogc_fid)
 );
-CREATE INDEX utah_state_boundary_ugrc_geom_geom_idx ON pawximity.utah_state_boundary_ugrc USING gist (geom);
 
-
+CREATE INDEX utah_state_boundary_ugrc_geom_geom_idx ON
+pawximity.utah_state_boundary_ugrc
+	USING gist (geom);
 -- pawximity.utah_streams_hydrography_ugrc definition
-
 -- Drop table
-
 -- DROP TABLE pawximity.utah_streams_hydrography_ugrc;
 
 CREATE TABLE pawximity.utah_streams_hydrography_ugrc (
 	ogc_fid serial4 NOT NULL,
-	geom public.geometry(multilinestring, 26912) NULL,
+	geom public.geometry(multilinestring,
+26912) NULL,
 	object_id int4 NULL,
 	permanent_identifier varchar NULL,
 	fdate varchar NULL,
@@ -206,4 +205,7 @@ CREATE TABLE pawximity.utah_streams_hydrography_ugrc (
 	submerged int4 NULL,
 	CONSTRAINT utah_streams_hydrography_ugrc_pk PRIMARY KEY (ogc_fid)
 );
-CREATE INDEX utah_streams_hydrography_ugrc_geom_geom_idx ON pawximity.utah_streams_hydrography_ugrc USING gist (geom);
+
+CREATE INDEX utah_streams_hydrography_ugrc_geom_geom_idx ON
+pawximity.utah_streams_hydrography_ugrc
+	USING gist (geom);
--- scripts/etl/transform_trails_salt_lake_clipped.sql
+++ scripts/etl/transform_trails_salt_lake_clipped.sql
@@ -1,40 +1,42 @@
-SELECT s.*,
-FROM pawximity.stg_trails_by_county s
-JOIN pawximity.utah_county_boundary_ugrc c ON s.county_id = c.ogc_fid
-WHERE s.county_name = 'SALT LAKE';
+SELECT
+    s.*,
+FROM
+    pawximity.stg_trails_by_county s
+    JOIN pawximity.utah_county_boundary_ugrc c ON s.county_id = c.ogc_fid
+WHERE
+    s.county_name = 'SALT LAKE';
 
 -- pawximity.trails_salt_lake_clipped definition
-
 -- Drop table
-
 -- DROP TABLE pawximity.trails_salt_lake_clipped;
-
 CREATE TABLE pawximity.trails_salt_lake_clipped (
-	id SERIAL PRIMARY KEY,
-	trail_id BIGINT, 
-	osm_id BIGINT, 
-	trail_type TEXT, 
-	trail_name TEXT, 
-	has_tunnel TEXT, 
-	county_id INTEGER, 
-	county_name TEXT, 
-	clipped_trail_geom GEOMETRY(MultiLineString, 26912)
+    id serial PRIMARY KEY,
+    trail_id bigint,
+    osm_id bigint,
+    trail_type text,
+    trail_name text,
+    has_tunnel text,
+    county_id integer,
+    county_name text,
+    clipped_trail_geom GEOMETRY(MultiLineString, 26912)
 );
-
 
 CREATE INDEX trails_salt_lake_clipped_geom_geom_idx ON pawximity.trails_salt_lake_clipped USING gist (clipped_trail_geom);
 
 -- TRUNCATE TABLE pawximity.trails_salt_lake_clipped;
-
 INSERT INTO pawximity.trails_salt_lake_clipped (trail_id, osm_id, trail_type, trail_name, has_tunnel, county_id, county_name, clipped_trail_geom)
-SELECT s.trail_id,
-       s.osm_id,
-       s.trail_type,
-       s.trail_name,
-       s.has_tunnel,
-       s.county_id,
-       s.county_name,
-       ST_Intersection(s.trail_geom, c.geom)::geometry(MultiLineString, 26912) AS clipped_trail_geom
-FROM pawximity.stg_trails_by_county s
-JOIN pawximity.utah_county_boundary_ugrc c ON s.county_id = c.ogc_fid
-WHERE s.county_name = 'SALT LAKE';
+SELECT
+    s.trail_id,
+    s.osm_id,
+    s.trail_type,
+    s.trail_name,
+    s.has_tunnel,
+    s.county_id,
+    s.county_name,
+    ST_Intersection (s.trail_geom, c.geom)::geometry(MultiLineString, 26912) AS clipped_trail_geom
+FROM
+    pawximity.stg_trails_by_county s
+    JOIN pawximity.utah_county_boundary_ugrc c ON s.county_id = c.ogc_fid
+WHERE
+    s.county_name = 'SALT LAKE';
+
