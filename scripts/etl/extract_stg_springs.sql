2da37a3a348bdb72e7a9f54fb4c55730c3adabf6
pawximity <chrisloverchio+gis@gmail.com>
2025-07-20 00:09:29 -0600

    staging springs data

---
+++ scripts/etl/extract_stg_springs.sql
@@ -0,0 +1,25 @@
+-- pawximity.stg_springs definition
+-- Drop table
+-- DROP TABLE pawximity.stg_springs
+CREATE TABLE pawximity.stg_springs (
+    id serial PRIMARY KEY,
+    spring_id integer,
+    spring_type text,
+    spring_name text,
+    spring_geom GEOMETRY(MultiPoint, 26912)
+);
+
+CREATE INDEX stg_springs_geom_geom_idx ON pawximity.stg_springs USING gist (spring_geom);
+
+-- TRUNCATE TABLE pawximity.stg_springs;
+INSERT INTO pawximity.stg_springs (spring_id, spring_type, spring_name, spring_geom)
+SELECT
+    ogc_fid AS id,
+    ftype_text AS spring_type,
+    gnis_name AS spring_name,
+    ST_Multi (geom)::geometry(MultiPoint, 26912)
+FROM
+    pawximity.utah_springs_hydrography_ugrc u
+WHERE
+    inutah = 1;
+
